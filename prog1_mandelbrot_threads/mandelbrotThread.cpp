bc5cfa89f74651a39449d78d2d71578928e0c79e
xyl <lrel7@mail.ustc.edu.cn>
2023-06-07 16:08:22 +0800

    finished program1

--- prog1_mandelbrot_threads/mandelbrotThread.cpp
+++ prog1_mandelbrot_threads/mandelbrotThread.cpp
@@ -12,6 +12,12 @@ typedef struct {
     int* output;
     int threadId;
     int numThreads;
+
+    // add
+    int startRow;
+    int numRows;
+    int startCol;
+    int numCols;
 } WorkerArgs;
 
 
@@ -19,6 +25,13 @@ extern void mandelbrotSerial(
     float x0, float y0, float x1, float y1,
     int width, int height,
     int startRow, int numRows,
+    int maxIterations,
+    int output[]);
+
+extern void mandelbrotSerial_v2(
+    float x0, float y0, float x1, float y1,
+    int width, int height,
+    int startRow, int step,
     int maxIterations,
     int output[]);
 
@@ -32,9 +45,16 @@ void* workerThreadStart(void* threadArgs) {
     WorkerArgs* args = static_cast<WorkerArgs*>(threadArgs);
 
     // TODO: Implement worker thread here.
+    double start_time = CycleTimer::currentSeconds();
+    // mandelbrotSerial(args->x0, args->y0, args->x1, args->y1, args->width, 
+    // args->height, args->startRow, args->numRows, args->maxIterations, args->output);
 
-    printf("Hello world from thread %d\n", args->threadId);
+    mandelbrotSerial_v2(args->x0, args->y0, args->x1, args->y1, args->width, 
+    args->height, args->threadId, args->numThreads, args->maxIterations, args->output);
 
+    // printf("Hello world from thread %d\n", args->threadId);
+    double end_time = CycleTimer::currentSeconds();
+    printf("Thread%d: %.3f ms\n", args->threadId, (end_time-start_time)*1000);
     return NULL;
 }
 
@@ -50,6 +70,7 @@ void mandelbrotThread(
     int maxIterations, int output[])
 {
     const static int MAX_THREADS = 32;
+    int rows_per_thread = height / numThreads; // rows for each thread to calculate
 
     if (numThreads > MAX_THREADS)
     {
@@ -63,6 +84,26 @@ void mandelbrotThread(
     for (int i=0; i<numThreads; i++) {
         // TODO: Set thread arguments here.
         args[i].threadId = i;
+        // directly copy
+        args[i].x0 = x0;
+        args[i].x1 = x1;
+        args[i].y0 = y0;
+        args[i].y1 = y1;
+        args[i].width = width;
+        args[i].height = height;
+        args[i].maxIterations = maxIterations;
+        args[i].output = output;
+        args[i].numThreads = numThreads;
+
+        // assign
+        /*args[i].startRow = i * rows_per_thread;
+        if(i < numThreads-1){
+            args[i].numRows = rows_per_thread;
+        }
+        else{
+            // the last thread has to complete all the rest of the work
+            args[i].numRows = height - rows_per_thread * (numThreads-1);
+        }*/
     }
 
     // Fire up the worker threads.  Note that numThreads-1 pthreads
